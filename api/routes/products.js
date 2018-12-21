f1d85615344af0e6a776cfa48a8d373267b38529
gem007bd <gem007bd@yahoo.com>
2018-12-21 16:51:33 +0100

    add mongoose atlas

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,11 +11,18 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(201).json({
+    });
+    product
+        .save()
+        .then(result => {
+        console.log(result);
+        })
+        .catch(err => log(err));
+    res.status(201).json({ 
         message: 'Handling POST request to /products',
         createdProduct: product
     });
@@ -20,17 +30,18 @@ router.post('/', (req, res, next) => {
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discover the special Id', 
-            id: id
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            res.status (200).json(doc);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err   
+            })
         });
-    } else {
-        res.status(200).json({
-            message: 'You passed and ID',
-            id: id
-        });
-    }
 });
 
 router.patch('/:productId', (req, res, next) => {
--- app.js
+++ app.js
@@ -2,27 +2,44 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb+srv://gem007bd:'+ 
+    process.env.MONGO_ATLAS_PW +
+    '@cluster0-inwg1.mongodb.net/test?retryWrites=true',
+    {
+        useNewUrlParser: true
+    });
+
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+/**
+ * parsing the body
+ */
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
+// Handling Cross-Origin Resource Sharing(CROS) Errors
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-        );
+        'Origin,  X-Requested-With, Content-Type, Accept, Authorization'
+    );
     if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE');
-        return res.status(200).json({});
+        res.header(
+            'Access-Control-Allow-Methods',
+            'PUT, POST, PATCH, DELETE, GET'
+            );
+        return res.status.json({});
     }
+    next();
 });
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
